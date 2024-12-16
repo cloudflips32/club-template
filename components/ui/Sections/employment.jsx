3df5926c406edf10ea79c5759c39e4d337115b4a
cloudflips32 <devadamstandish@gmail.com>
2024-12-15 21:51:40 -0500

    margin adjustments, employment component

--- components/ui/Sections/employment.jsx
+++ components/ui/Sections/employment.jsx
@@ -86,7 +86,7 @@ const Employment = () => {
   return (
     <section id="employment" className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl md:text-5xl sm:text-4xl font-bold text-center mb-8">Potential Employers Include:</h2>
+        <h2 className="text-3xl md:text-5xl sm:text-4xl font-bold text-center lg:mb-20 md:mb-20 mb-10">Potential Employers Include:</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
           {randomEmployers.map((company) => (
             <div key={company.name} className="bg-white content-center items-center space-around p-6 rounded-lg shadow-md">
