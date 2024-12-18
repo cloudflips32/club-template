7a09fd4e3c5095434a5e0b938c9c4d6883c73f29
cloudflips32 <devadamstandish@gmail.com>
2024-12-18 01:03:48 -0500

    key set to id

--- components/ui/Sections/employment.jsx
+++ components/ui/Sections/employment.jsx
@@ -37,7 +37,7 @@ const Employment = () => {
         <h2 className="text-3xl md:text-5xl sm:text-4xl font-bold text-center lg:mb-20 md:mb-20 mb-10">Potential Employers Include:</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
           {employers.map((employer) => (
-            <div key={employer.name} className="bg-white content-center items-center space-around p-6 rounded-lg shadow-md">
+            <div key={employer.id} className="bg-white content-center items-center space-around p-6 rounded-lg shadow-md">
             <Link className="flex flex-col items-left justify-center text-gray-600 text-sm text-center" href={employer.recruitLink} aria-label="Find Your Career">
               <img src={employer.logo} alt={`${employer.name} Logo`} width={40} height={40} className="mx-auto my-2 mb-4" />
               <h3 className="text-xl font-medium mb-2 text-center">{employer.name}</h3>
