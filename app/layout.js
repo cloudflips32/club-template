0dace2aa0ea676f215b0bd7857b50d666b2e3835
cloudflips32 <devadamstandish@gmail.com>
2024-12-19 21:10:24 -0500

    vercel speed insights added, additional analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights/>
         <Toaster />
       </body>
     </html>
