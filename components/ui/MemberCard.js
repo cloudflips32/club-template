8ed973ea993b4fc7ab13730be452faa0bab40398
cloudflips32 <devadamstandish@gmail.com>
2024-12-22 20:55:42 -0500

    small edits

--- components/ui/MemberCard.js
+++ components/ui/MemberCard.js
@@ -26,7 +26,7 @@ function MemberCard() {
   )
 
   return (
-<>
+    <>
       <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
         <div className="container px-4 md:px-6 mx-auto">
           <h2 className="
